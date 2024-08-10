f56d6af066058dff3325bb0e76b5748afd460175
Kauã Gabriel <133836017+devPxT@users.noreply.github.com>
2024-08-10 20:03:52 -0300

    Add files via upload

---
+++ src/StaticStack.java
@@ -0,0 +1,44 @@
+public class StaticStack<T> {
+    private int top = -1;
+    private T[] data;
+
+    public StaticStack(int size) {
+        this.data = (T[]) new Object[size];
+    }
+
+    public void push(T data) {
+        if (top == this.data.length - 1) {
+            throw new IllegalStateException("Stack is full");
+        }
+        top++;
+        this.data[top] = data;
+    }
+
+    public T pop() {
+        if (top == -1) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        T value = this.data[top];
+        this.data[top] = null;
+        top--;
+        return value;
+    }
+
+    public void clear() {
+        
+    }
+
+    public boolean isFull() {
+        if (top == this.data.length - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEmpty() {
+        if (this.top == -1) {
+            return true;
+        }
+        return false;
+    }
+}
